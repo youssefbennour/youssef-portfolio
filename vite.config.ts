3d885436140ce74ff24e4bc83cbe18cc8eaff757
youssefbennour <youssefbennour15@gmail.com>
2025-07-17 15:54:50 +0100

    FIX: change path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/youssefbennour",
+  base: "/youssef-portfolio",
   plugins: [
     react(),
     mode === 'development' &&
